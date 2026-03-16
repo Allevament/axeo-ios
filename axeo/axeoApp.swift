647a0354a952e36a5957db01b17ad34427cfe2e2
Boris Volkov <allevamentum@icloud.com>
2026-03-17 03:09:15 +0500

    Initial Commit

---
+++ axeo/axeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  axeoApp.swift
+//  axeo
+//
+//  Created by Boris Volkov on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct axeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
